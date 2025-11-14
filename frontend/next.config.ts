187a085a5ccb666c1661190f5222cfa956b79c14
Eren Taskiran <eren.tas3535@gmail.com>
2025-11-15 00:20:43 +0300

    Configure Next.js for remote image hosting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+    remotePatterns: [new URL('https://www.groundguitar.com/wp-content/uploads/2024/02/Nirvana-Nevermind-Coolest-Rock-Album-Covers-1024x1024.jpeg')],
+  },
 };
 
 export default nextConfig;
